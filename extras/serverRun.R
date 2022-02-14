a10add17d536d8aea10f16014ce6e0bbd8aac929
fanbu1995 <fb75@duke.edu>
2022-02-14 12:14:03 -0800

    run fewer historical comparator analyses

--- extras/serverRun.R
+++ extras/serverRun.R
@@ -16,18 +16,27 @@ output_dir = '/u/home/f/fanbu42/betterResults'
 ## try CCAE, Feb 12
 database_id = 'CCAE'
 
+## move on to MDCR
+database_id = 'IBM_MDCR'
+
 ## process array_id to figure out what to run in this task ---------
 array_id = Sys.getenv('SGE_TASK_ID') %>% as.numeric()
 # SCCS: analysis_id 1-15; period_id 1-12
 # HC: analysis_id 1-24;  period_id 1-12
+# Feb 14 update: only do analysis 1-12 for HC (13-24 has filtered outcomes)
 
 ### Feb 9 update: split it further up to smaller chunks
 ### with 300 runs
 
-NR = 300
+### Feb 14: 240 runs in total (48 * 5)
+
+total_analyses = 48 #60
+groups = 5 # 20 exposures per group, 10 expos total
+
+NR = total_analyses * groups
 runs = c(1:NR)
-expo_groups = (runs-1) %/% 60 + 1
-analysis_groups = rep(1:60, NR%/%60)
+expo_groups = (runs-1) %/% total_analyses + 1
+analysis_groups = rep(1:total_analyses, NR%/%total_analyses)
 
 exid = expo_groups[array_id]
 aid = analysis_groups[array_id]
@@ -46,13 +55,19 @@ if(aid <= 24){
   ## last 36: HC
   method = "HistoricalComparator"
   period_id = (aid-24 + 2) %/% 3
-  #cat('aid =', aid, '; period_id =', period_id, '\n')
-  if(aid %% 3 == 1){
-    analysis_ids = c(1:8)
-  }else if(aid %% 3 == 2){
-    analysis_ids = c(9:16)
+  # if(aid %% 3 == 1){
+  #   analysis_ids = c(1:8)
+  # }else if(aid %% 3 == 2){
+  #   analysis_ids = c(9:16)
+  # }else{
+  #   analysis_ids = c(17:24)
+  # }
+  ## Feb 14 update: only do analysis 1-12
+  if(aid %% 2 == 1){
+    # odd number: analysis 1-7
+    analysis_ids = c(1:6)
   }else{
-    analysis_ids = c(17:24)
+    analysis_ids = c(7:12)
   }
 }
 
