32b67f3bc2b8ff4fe99482d70a0330c25294ae27
fanbu1995 <fb75@duke.edu>
2022-04-06 22:24:32 -0700

    batch produce decisions

--- extras/GBSpostprocess.R
+++ extras/GBSpostprocess.R
@@ -3,6 +3,7 @@
 
 source('./extras/postProcessUtils.R')
 source('./R/helperFunctions.R')
+source('./extras/simpleCalibration.R')
 
 
 resultspath = '~/Documents/Research/better_gbs/summary/'
@@ -15,7 +16,7 @@ allRes = pullGBSResults(database_id = db,
                         exposure_ids = expos,
                         resultsPath = resultspath)
 
-# produce some examples first
+# produce some examples first -----
 exRes1 = allRes %>% 
   filter(method == 'HistoricalComparator',
          analysis_id == 2,
@@ -31,3 +32,91 @@ exRes2 = allRes %>%
          prior_id == 1)
 decision2.1 = getOverallDecisions(exRes1, d1 = 0.977, d0 = 0.9, withEstimates = TRUE)
 decision2.2 = getOverallDecisions(exRes1, d1 = 0.994, d0 = 0.9, withEstimates = TRUE)
+## none of the results are significant!!
+
+
+# function to mass-produce decisions
+getAllDecisionsWithCalibration <- function(res,
+                                           calibrate = TRUE,
+                                           estimatesPath = NULL,
+                                           alpha = 0.05,
+                                           adjust = TRUE,
+                                           defaults = list(d1=0.95, d0=0.9),
+                                           estimateType = 'Mean',
+                                           maxCores = 4){
+  if(calibrate & is.null(estimatesPath)){
+    stop('estaimtesPath must be provided if calibrate!!')
+  }
+  
+  d1 = defaults$d1; d0 = defaults$d0
+  
+  # sub-function to process 
+  decideChunk <- function(chunk){
+    db = chunk$database_id[1]
+    me = chunk$method[1]
+    aid = chunk$analysis_id[1]
+    expo = chunk$exposure_id[1]
+    prid = chunk$prior_id[1]
+    
+    # get calibrated delta1
+    if(calibrate){
+      calibrateRes = calibrateByDelta1(db, me, aid, expo, prid, estimatesPath, 
+                                       alpha = alpha, useAdjusted = adjust, evalType2 = FALSE)
+      if(is.null(calibrateRes)){
+        cat('No calibrated threshold produced!\n')
+        return(NULL)
+      }
+      d1 = calibrateRes$calibratedDelta1
+    }
+    
+    # decide
+    chunk.decision = getOverallDecisions(chunk, d1, d0, 
+                                         withEstimates = TRUE, 
+                                         estimateType = estimateType)
+    
+    sel.decision = chunk.decision %>% select(!starts_with('adjusted')) 
+    if(adjust){
+      decision = chunk.decision %>% select(starts_with('adjusted'))
+      names(decision) = names(sel.decision)
+    }else{
+      decision= sel.decision
+    }
+    
+    decision = decision %>% 
+      mutate(database_id = db,
+             method = me, 
+             analysis_id = aid,
+             exposure_id = expo, 
+             prior_id = prid)
+    
+    return(decision)
+  }
+  
+  # run in parallel
+  chunks = split(res,
+                 list(
+                   res$database_id,
+                   res$method,
+                   res$analysis_id,
+                   res$exposure_id,
+                   res$prior_id
+                 ), drop=TRUE)
+  
+  cluster = ParallelLogger::makeCluster(min(maxCores, 4))
+  resls = ParallelLogger::clusterApply(cluster, chunks, decideChunk,
+                               stopOnError = FALSE, progressBar = TRUE)
+  ParallelLogger::stopCluster(cluster)
+  AllDec = bind_rows(resls)
+  
+  return(AllDec)
+}
+
+
+## try batch processing
+estimatespath = '~/Documents/Research/betterResults/summary/'
+AllDecAdj = getAllDecisionsWithCalibration(allRes %>% filter(!is.na(adjustedPostMean)),
+                                           calibrate = TRUE,
+                                           estimatesPath = estimatespath,
+                                           maxCores = 1)
+
+calibrateByDelta1('CCAE', 'HistoricalComparator', 1,1,1, estimatespath, useAdjusted = TRUE)
