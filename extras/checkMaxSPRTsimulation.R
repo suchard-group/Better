51c1db6933e32b9a4d61e9c8c4665d275cba6605
fanbu1995 <fb75@duke.edu>
2022-07-15 16:11:40 -0700

    add Bayesian testing simulations

--- extras/checkMaxSPRTsimulation.R
+++ extras/checkMaxSPRTsimulation.R
@@ -2,6 +2,7 @@
 # simulation to check error rates of MaxSPRT
 
 library(Sequential)
+library(dplyr)
 
 # use a Poisson model first
 # 1. MaxSPRT -------
@@ -92,7 +93,7 @@ for(s in 1:S){
   allRejects = allRejects + reject
 }
 
-(Type1error = allRejects/S)
+(Type1error = allRejects/S) # 0.006
 
 
 # (2) effect = 1.5
@@ -194,18 +195,18 @@ simulatePoissonBayes <- function(S, numLooks=12,
       ## get P1
       this.P1 = pgamma(expectedRate, 
                        shape = postAlpha, rate = postBeta,
-                       upper = TRUE) #???
+                       lower.tail = FALSE) #evaluate the upper tail prob
       
       ## save the P1 entry
       ind = (s-1)*numLooks + i
-      P1s[ind] = P1
+      P1s[ind] = this.P1
       
       ## decision making using delta_1 = 1-alpha
-      if(P1 > 1-alpha){
+      if(this.P1 > 1-alpha){
         Reject[ind] = TRUE
         if(!flag){
           flag = TRUE
-          cat(sprintf('\nNull rejected at data look %s!\n'), i)
+          cat(sprintf('\nNull rejected at data look %s!\n',i))
         }
       }
 
@@ -220,3 +221,98 @@ simulatePoissonBayes <- function(S, numLooks=12,
   
   return(res)
 }
+
+## another function to make decision and check rejection rate
+getRejectionRate <- function(res, alpha = 0.05){
+  
+  if(alpha != 0.05){
+    res$Reject = res$P1 > 1-alpha
+  }
+  
+  Decisions = res %>% group_by(Simulation) %>%
+    summarize(overallReject = any(Reject)) %>%
+    ungroup()
+  
+  ## sanity check
+  cat(sprintf('Out of %s total simulations, %s final decision are reached.\n', 
+              max(res$Simulation), nrow(Decisions)))
+  
+  mean(Decisions$overallReject)
+}
+
+## try it
+
+nL = 12
+Nt = 60
+eC = 3
+
+S = 5000
+
+pAlpha = 0.01
+pBeta = 0.01
+alphaDefault = 0.05
+
+set.seed(42)
+
+# (1) effect = 1
+
+effect = 1
+
+sims1 = simulatePoissonBayes(S, numLooks = nL,
+                            Nbatch = Nt, 
+                            expectedCount = eC,
+                            effectSize = effect,
+                            priorAlpha = pAlpha,
+                            priorBeta = pBeta,
+                            alpha = alpha)
+
+getRejectionRate(sims1, alpha = 0.0155) # calibrated at 0.05; 0.0492
+
+
+# (2) effect = 1.5
+
+effect = 1.5
+
+sims15 = simulatePoissonBayes(S, numLooks = nL,
+                             Nbatch = Nt, 
+                             expectedCount = eC,
+                             effectSize = effect,
+                             priorAlpha = pAlpha,
+                             priorBeta = pBeta,
+                             alpha = alpha)
+
+1 - getRejectionRate(sims15, alpha = 0.0155) # errorRate = 0.2126
+
+
+# (3) effect = 2
+
+effect = 2
+
+sims20 = simulatePoissonBayes(S, numLooks = nL,
+                              Nbatch = Nt, 
+                              expectedCount = eC,
+                              effectSize = effect,
+                              priorAlpha = pAlpha,
+                              priorBeta = pBeta,
+                              alpha = alpha)
+
+1 - getRejectionRate(sims20, alpha = 0.0155) # errorRate = 0.0018
+
+
+# (4) effect = 4
+
+effect = 4
+
+sims40 = simulatePoissonBayes(S, numLooks = nL,
+                              Nbatch = Nt, 
+                              expectedCount = eC,
+                              effectSize = effect,
+                              priorAlpha = pAlpha,
+                              priorBeta = pBeta,
+                              alpha = alpha)
+
+1 - getRejectionRate(sims40, alpha = 0.0155) # errorRate = 0.0
+
+
+
+
