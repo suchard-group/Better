f3478cf6752afbfc91d90f1d3779398a31f13746
fanbu1995 <fb75@duke.edu>
2022-07-14 22:32:13 -0700

    MaxSPRT simulation

---
+++ extras/checkMaxSPRTsimulation.R
@@ -0,0 +1,152 @@
+# 07/14/2022
+# simulation to check error rates of MaxSPRT
+
+library(Sequential)
+
+# use a Poisson model first
+
+## a function to simulate Poisson data and run sequential test
+simulatePoissonTest <- function(S, numLooks=12, 
+                                Nbatch=100, 
+                                expectedCount = 5,
+                                effectSize = 1, 
+                                alphaSpend = 'Wald',
+                                cachePath = './localCache/'){
+  
+  rejects = 0
+  
+  for(s in 1:S){
+    cat(sprintf('Simulation round %s:...\n', s))
+    testName = sprintf('Poisson%s', s)
+    
+    if(file.exists(file.path(cachepath, sprintf('%s.txt',testName)))){
+      file.remove(file.path(cachepath, sprintf('%s.txt',testName)))
+    }
+    
+    AnalyzeSetUp.Poisson(name = testName, 
+                         SampleSize = expectedCount * numLooks,
+                         M = 4, # recommended by Kulldorff and Silva
+                         AlphaSpendType = alphaSpend,
+                         address = cachePath)
+    
+    #AllRes = NULL
+    
+    for(i in 1:numLooks){
+      ## simulate Poisson count data
+      obsCounts = sum(rpois(Nbatch, expectedCount/Nbatch*effectSize))
+      
+      ## test
+      res = Analyze.Poisson('Poisson1', test = i, 
+                            mu0 = expectedCount, 
+                            events = obsCounts)
+      
+      if(res[nrow(res),'Reject H0'] == 'Yes'){
+        cat(sprintf('Null rejected at %sth data look.\n', i))
+        rejects = rejects + 1
+        break
+      }
+    }
+    
+    rm()
+  }
+  
+  return(rejects)
+}
+
+
+## try simulation run
+
+cachepath = '~/Documents/Research/better/localCache/'
+
+# rejects = simulatePoissonTest(1, numLooks = 12, 
+#                               Nbatch = 100,
+#                               expectedCount = 5,
+#                               effectSize = 5, 
+#                               alphaSpend = 'Wald',
+#                               cachePath = cachepath)
+# somehow there is an internal stuff that doesn't allow me to repeat testing inside the function???
+
+## do this manually
+set.seed(42)
+
+## basic settings
+S = 100
+nL = 12
+Nt = 20
+eC = 1
+alphaSpendType = 'Wald'
+
+
+## under H0:
+effect = 1
+allRejects = 0
+
+for(s in 1:S){
+  cat(sprintf('\n\nROUND %s...\n\n', s))
+  reject = simulatePoissonTest(1, numLooks = nL,
+                               Nbatch = Nt,
+                               expectedCount = eC,
+                               effectSize = effect,
+                               alphaSpend = alphaSpendType,
+                               cachePath = cachepath)
+  allRejects = allRejects + reject
+}
+
+(Type1error = allRejects/S) #0.06
+
+
+## under H1: effectSize = 1.5
+effect = 1.5
+allRejects = 0
+
+for(s in 1:S){
+  cat(sprintf('\n\nROUND %s...\n\n', s))
+  reject = simulatePoissonTest(1, numLooks = nL,
+                               Nbatch = Nt,
+                               expectedCount = eC,
+                               effectSize = effect,
+                               alphaSpend = alphaSpendType,
+                               cachePath = cachepath)
+  allRejects = allRejects + reject
+}
+
+(Type2error15 = 1 - allRejects/S) #0.07 0.62
+
+
+## under H1: effectSize = 2
+effect = 2
+allRejects = 0
+
+for(s in 1:S){
+  cat(sprintf('\n\nROUND %s...\n\n', s))
+  reject = simulatePoissonTest(1, numLooks = nL,
+                               Nbatch = Nt,
+                               expectedCount = eC,
+                               effectSize = effect,
+                               alphaSpend = alphaSpendType,
+                               cachePath = cachepath)
+  allRejects = allRejects + reject
+}
+
+(Type2error20 = 1 - allRejects/S) # ... 0.1
+
+
+## under H1: effectSize = 4
+effect = 4
+allRejects = 0
+
+for(s in 1:S){
+  cat(sprintf('\n\nROUND %s...\n\n', s))
+  reject = simulatePoissonTest(1, numLooks = nL,
+                               Nbatch = Nt,
+                               expectedCount = eC,
+                               effectSize = effect,
+                               alphaSpend = alphaSpendType,
+                               cachePath = cachepath)
+  allRejects = allRejects + reject
+}
+
+(Type2error40 = 1 - allRejects/S) # 0 0
+
+
+
