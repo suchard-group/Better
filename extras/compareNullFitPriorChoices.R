d0a685100361ece0543b6ff60ba0862c961e20f8
fanbu1995 <fb75@duke.edu>
2022-07-29 21:44:13 -0700

    compare different priors for negative control meta analysis

---
+++ extras/compareNullFitPriorChoices.R
@@ -0,0 +1,108 @@
+# July 2022
+# compare results with different prior choices for negative control analysis
+# (using Bayesian meta analysis)
+
+source('./extras/postProcessUtils.R')
+source('./extras/simpleCalibration.R')
+
+## be hacky about it for now
+## pretending we are dealing with ALL exposures
+exposures = readRDS('./localCache/exposures.rds')
+allExposure_ids = exposures$exposure_id
+
+## a function to compute error rates and show plots 
+summarizeAndPlotTempDelta1ByPrior <- function(database_id, method, exposures, rawPath, summaryPath, ...){
+  # pull results first
+  summ = pullResults(database_id, method, exposures, resultsPath = rawPath, savePath = summaryPath)
+  
+  # then get error rates results and plots
+  res_summ = plotTempDelta1ByPriors(database_id, method, summaryPath = summaryPath, ...)
+  
+  return(res_summ)
+}
+
+
+## specify database, method, analysis and exposure
+db = 'CCAE'
+#me = 'SCCS' 
+me = 'HistoricalComparator'
+aid = 6
+eid = 211981
+# summarypath = outputPath
+cachepath = './localCache/'
+
+
+## go through the different prior settings
+
+adjust = TRUE
+
+for(dir_suffix in c('default2', 'shrinkMu2', 'shrinkBoth2')){
+  if(dir_suffix == 'default2'){
+    title = 'muPriorSd = 2, tauPriorSd = 0.5'
+  }else if(dir_suffix == 'shrinkMu2'){
+    title = 'muPriorSd = 0.5, tauPriorSd = 0.5'
+  }else{
+    title = 'muPriorSd = 0.2, tauPriorSd = 0.2'
+  }
+  
+  resultPath = sprintf('~/Documents/Research/betterResults/betterResults-%s', dir_suffix)
+  outputPath = sprintf('~/Documents/Research/betterResults/summary-%s/', dir_suffix)
+  
+  res = summarizeAndPlotTempDelta1ByPrior(database_id = db,
+                                          method = me,
+                                          exposures = allExposure_ids,
+                                          rawPath = resultPath,
+                                          summaryPath = outputPath,
+                                          analysis_id = aid,
+                                          exposure_id = eid,
+                                          prior_ids = c(1:3), # include all priors for easier query later
+                                          alpha = 0.05,
+                                          cachePath = cachepath,
+                                          useAdjusted = adjust,
+                                          showPlots = FALSE,
+                                          stratifyByEffectSize = TRUE,
+                                          calibrate = FALSE, 
+                                          outcomesInEstimates = NULL)
+  
+  pp = attr(res, 'plot')
+  print(pp + labs(title = title))
+}
+
+## the prior choices only make some difference in earlier periods
+## but the endpoints look very similar!
+
+## perhaps sdPriors = c(0.5, 0.5) has the best performance
+## (in hitting 0.05 Type I and having reasonable power)
+
+
+
+##### not run-----
+## set suffix
+dir_suffix = 'default2'
+
+##
+
+
+
+## default prior setting 
+summDefault = pullResults(database_id = db,
+                          method = me,
+                          exposure_id = allExposure_ids,
+                          resultsPath = resultPath,
+                          savePath = outputPath)
+
+res_default_raw = plotTempDelta1ByPriors(database_id = db,
+                                         method = me, 
+                                         analysis_id = aid,
+                                         exposure_id = eid,
+                                         prior_ids = c(1:3), # include all priors for easier query later
+                                         alpha = 0.05,
+                                         summaryPath = summarypath,
+                                         cachePath = cachepath,
+                                         useAdjusted = FALSE,
+                                         showPlots = TRUE,
+                                         stratifyByEffectSize = TRUE,
+                                         calibrate = FALSE, 
+                                         outcomesInEstimates = NULL)
+
+
