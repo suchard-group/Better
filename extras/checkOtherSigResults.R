4565ae1dadaeb7a198186d5a2f101358ec877dca
fanbu1995 <fb75@duke.edu>
2022-10-10 11:38:45 -0700

    hunt for significant GBS results

--- extras/checkOtherSigResults.R
+++ extras/checkOtherSigResults.R
@@ -4,6 +4,12 @@
 
 # path and file name
 est_path = '~/Documents/Research/better_gbs_flu84/Results_MDCR/'
+est_path = '~/Documents/Research/better_gbs/Results_MDCR/'
+est_path = '~/Documents/Research/better_gbs/Results_MDCR-old/'
+
+est_path = '~/Documents/Research/better_gbs/Results_CCAE/'
+est_path = '~/Documents/Research/better_gbs/Results_CCAE-old/'
+
 fname = 'estimate.csv'
 
 fpath = file.path(est_path, fname)
@@ -23,3 +29,15 @@ res_final = res %>% group_by(method, analysisId, exposureId) %>%
 
 # look at "significant" results with CI > 1
 res_final %>% filter(ci95Lb >= 1)
+
+# (1) CCAE, 43-183 days for post control
+#     all flu vaccine, SCCS
+# method analysisId periodId exposureId outcomeId    rr ci95Lb ci95Ub
+# <chr>       <dbl>    <dbl>      <dbl>     <dbl> <dbl>  <dbl>  <dbl>
+# 1 SCCS            1        9      21215       343  2.03   1.02   3.74
+# 2 SCCS            2        9      21215       343  2.25   1.12   4.20
+# 3 SCCS            3        9      21215       343  9.73   1.87 178.  
+# 4 SCCS            7        9      21215       343  7.70   1.52 140.  
+
+# (2) CCAE, 43-365 (73?) days for post control
+# the "significant" results are all the same as in (1)...
