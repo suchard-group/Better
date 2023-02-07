8f0f0a178f93724c13e6f77095b11b8b917a6b75
fanbu1995 <fb75@duke.edu>
2023-02-07 13:59:14 -0800

    more simulation plots and contrast with real-world type 1 errors

--- extras/showMaxSPRTweaknessSimulation.R
+++ extras/showMaxSPRTweaknessSimulation.R
@@ -265,6 +265,7 @@ simulated1 = maxSprtPoisson(S = 500,
 Rates1 = getMaxSprtRejectRate(simulated1)
 
 ## (iii) actually planned out a 1-year surveillance, but had to do 2-year
+## 02/07/2023: modified; using exact same batch of data but different MaxSPRT choices
 set.seed(42)
 simulated2 = maxSprtPoisson(S = 500, 
                             numLooks=24, 
@@ -272,18 +273,19 @@ simulated2 = maxSprtPoisson(S = 500,
                             actualExpectedCount = 10,
                             effectSize = 1,
                             alpha = 0.05)
-Rates2 = getMaxSprtRejectRate(simulated2, numLooks = 12)
+Rates2_shorter = getMaxSprtRejectRate(simulated2, numLooks = 12)
 Rates2_valid = getMaxSprtRejectRate(simulated2, numLooks = 24)
 Rates3_longer = getMaxSprtRejectRate(simulated2, numLooks = 36)
 
 ## (iv) the Bayesian thing...
+set.seed(42)
 bayesSim = BayesianPoisson(S = 500,
                            numLooks = 24,
                            expectedCount = 10,
-                           delta1 = 0.98)
+                           delta1 = 0.987)
 RatesB = getBayesRejectionRate(bayesSim,
                                numLooks = 24,
-                               delta1 = .98)
+                               delta1 = .987)
 
 
 ## (4) make plots for this very stupid hypothetical experiment----
@@ -431,3 +433,231 @@ ggplot(allRates, aes(x=Period, y=rejectRate, color = type)) +
                                 'Bayesian')) +
   theme_bw(base_size = 16)+
   theme(legend.position = c(0.8,0.35))
+
+
+# (8). other plots with only shorter/longer model violations ----
+# 02/07/2023 update
+# Data looks NOT by calendar time, BUT in person-time (num of expected incidents)
+pdf('~/Documents/Research/betterResults/plots/maxSPRT-weakness-simulation-updated.pdf',
+    height = 4, width = 7.5)
+
+type1colors = wes_palette("GrandBudapest1")[c(1,4,3)]
+
+## (i) everything goes according to plan (24 data looks)
+allRates = Rates2_valid$rejectByPeriod %>% mutate(type = 'A')
+
+ggplot(allRates, aes(x=Period, y=rejectRate)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  # geom_vline(xintercept = 12, color = 'brown', alpha = 0.6,
+  #            size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5, color = type1colors[1]) +
+  labs(x='Data looks (10 expected incidents per look)', y = 'Type 1 error rate') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,0.1), breaks = c(0,0.05, 0.1)) +
+  scale_color_manual(values = type1colors) +
+  theme_bw(base_size = 16)
+
+## (ii) if shorter surveillance were planned out (hacky invalid extension)
+allRates = allRates %>% bind_rows(Rates2_shorter$rejectByPeriod %>% mutate(type = 'B'))
+
+ggplot(allRates, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = 12, color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5) +
+  labs(x='Data looks (10 expected incidents per look)', y = 'Type 1 error rate') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,0.1), breaks = c(0,0.05, 0.1)) +
+  scale_color_manual(values = type1colors) +
+  theme_bw(base_size = 16)+
+  theme(legend.position = 'none')
+
+## (iii) if longer surveillance were planned out (early stopping)
+allRates = allRates %>% bind_rows(Rates3_longer$rejectByPeriod %>% mutate(type = 'C'))
+
+ggplot(allRates, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = c(12,24), color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5) +
+  labs(x='Data looks (10 expected incidents per look)', y = 'Type 1 error rate') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,0.1), breaks = c(0,0.05, 0.1)) +
+  scale_color_manual(values = type1colors) +
+  theme_bw(base_size = 16)+
+  theme(legend.position = 'none')
+
+## (iv) Bayesian performance
+type1colors = c(type1colors, wes_palette("Darjeeling2")[2])
+allRates = allRates %>% bind_rows(RatesB$rejectByPeriod %>% mutate(type = 'D')) 
+
+allRates = allRates %>%
+  mutate(shade = if_else(type!='D', 0.1, 1))
+
+ggplot(allRates, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = c(12,24), color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5, aes(alpha = shade)) +
+  labs(x='Data looks (10 expected incidents per look)', y = 'Type 1 error rate') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,0.1), breaks = c(0,0.05, 0.1)) +
+  scale_color_manual(values = type1colors) +
+  scale_alpha_continuous(range = c(0.45,1))+
+  theme_bw(base_size = 16)+
+  theme(legend.position = 'none')
+
+dev.off()
+
+# (9) examine power with RR=1.2 again, with de-bugged version
+pdf('~/Documents/Research/betterResults/plots/maxSPRT-Bayesian-simulation-error-power.pdf',
+    height = 4, width = 7.5)
+
+effect_size = 1.2
+
+## (iii) actually planned out a 1-year/3-year surveillance, but actually do 2-year
+simulated4 = maxSprtPoisson(S = 500, 
+                            numLooks=24, 
+                            expectedCountByPlan = 10,
+                            actualExpectedCount = 10,
+                            effectSize = effect_size,
+                            alpha = 0.05)
+Rates4_shorter = getMaxSprtRejectRate(simulated4, numLooks = 12)
+Rates4_valid = getMaxSprtRejectRate(simulated4, numLooks = 24)
+Rates4_longer = getMaxSprtRejectRate(simulated4, numLooks = 36)
+
+## (iv) the Bayesian thing...
+bayesSim = BayesianPoisson(S = 500,
+                           numLooks = 24,
+                           expectedCount = 10,
+                           effectSize = effect_size,
+                           delta1 = 0.987)
+RatesB_pos = getBayesRejectionRate(bayesSim,
+                               numLooks = 24,
+                               delta1 = .987)
+
+allRates_pos = bind_rows(Rates4_valid$rejectByPeriod %>% mutate(type = 'A'),
+                     Rates4_shorter$rejectByPeriod %>% mutate(type = 'B'),
+                     Rates4_longer$rejectByPeriod %>% mutate(type = 'C'),
+                     RatesB_pos$rejectByPeriod %>% mutate(type = 'D'))
+
+powerColors = c(wes_palette("GrandBudapest1")[c(1,4,3)], 
+                wes_palette("Darjeeling2")[2])
+
+ggplot(allRates_pos, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = 12, color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(size = 1.5) +
+  labs(x='Data looks (10 expected incidents per look)',
+       y = 'Statistical Power',
+       caption = 'Statistical power for RR = 1.2 signal detection',
+       color = 'Power for:') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,1), 
+                     breaks = seq(from = 0, to = 1, by = 0.25)) +
+  scale_color_manual(values = powerColors,
+                     labels = c('MaxSPRT (oracle)',
+                                'MaxSPRT (hacky extension)',
+                                'MaxSPRT (early stop)',
+                                'Bayesian')) +
+  theme_bw(base_size = 16)+
+  theme(legend.position = c(0.2,0.75),
+        legend.text = element_text(size = 10))
+
+## also re-produce the simulated Type 1 error plots from RR=1 experiments
+
+ggplot(allRates, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = c(12,24), color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5) +
+  labs(x='Data looks (10 expected incidents per look)', 
+       y = 'Type 1 error rate',
+       color = 'Type 1 error of:') +
+  scale_x_continuous(limits = c(0.5,24), breaks = seq(from=3,to=24,by=3)) +
+  scale_y_continuous(limits = c(0,0.1), breaks = c(0,0.05, 0.1)) +
+  scale_color_manual(values = type1colors,
+                     labels = c('MaxSPRT (oracle)',
+                                'MaxSPRT (hacky extension)',
+                                'MaxSPRT (early stop)',
+                                'Bayesian')) +
+  theme_bw(base_size = 16)+
+  theme(legend.position = c(0.2,0.75),
+        legend.text = element_text(size = 10))
+
+dev.off()
+
+
+# (10) for presentation purposes ----
+# plot 3 maxSPRT Type 1 error curves 
+# AND also real-world heavy inflated MaxSPRT type 1 error
+# to contrast...
+
+allRates_maxsprt = allRates %>% 
+  filter(type != 'D', Period <= 12) %>% 
+  select(-shade) 
+
+## obtain MaxSPRT emipircal real-world error rates
+source('./extras/frequentistDecisionComparisons.R')
+
+db = 'CCAE'
+eid = 211983 # Zoster two doses
+me = 'HistoricalComparator'
+aid = 4
+
+cachepath = './localCache/'
+maxSPRT_filepath = './localCache/EstimateswithImputedPcs_CCAE.rds'
+maxSPRT_estimates = readRDS(maxSPRT_filepath)
+
+resLst = frequentistDecisions(connection,
+                              'eumaeus',
+                              database_id = db,
+                              method = me,
+                              exposure_id = eid,
+                              analysis_id = aid,
+                              calibration = FALSE,
+                              correct_shift = TRUE,
+                              cachePath = cachepath,
+                              estimates = maxSPRT_estimates)
+
+maxsprt_errors = resLst$errorRate %>%
+  filter(effect_size == 1) %>%
+  select(Period = period_id, rejectRate = errorRate) %>%
+  mutate(type = 'E')
+
+allRates_maxsprt = allRates_maxsprt %>% bind_rows(maxsprt_errors)
+
+type1colors = c(type1colors[1:3], wes_palette("Darjeeling1")[5])
+
+ggplot(allRates_maxsprt, aes(x=Period, y=rejectRate, color = type)) +
+  geom_hline(yintercept = .05, color = 'gray60', 
+             size = 1, linetype=2)+
+  geom_vline(xintercept = c(12,24), color = 'brown', alpha = 0.6,
+             size = 1.2, linetype=2)+
+  geom_line(linewidth = 1.5) +
+  labs(x='Data looks', 
+       y = 'Type 1 error rate',
+       color = 'Type 1 error of:') +
+  scale_x_continuous(limits = c(0.5,12), breaks = seq(from=3,to=12,by=3)) +
+  scale_y_continuous(limits = c(0,0.5), breaks = c(0, 0.05, 0.1, 0.25, 0.3, 0.5)) +
+  scale_color_manual(values = type1colors,
+                     labels = c('MaxSPRT (oracle)',
+                                'MaxSPRT (hacky extension)',
+                                'MaxSPRT (early stop)',
+                                'MaxSPRT (real-world)')) +
+  theme_bw(base_size = 16)+
+  theme(legend.position = c(0.2,0.75),
+        legend.text = element_text(size = 10))
+
+
+
+
+
+
