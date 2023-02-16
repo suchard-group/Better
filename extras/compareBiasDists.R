c0b49d19203c7c8d385f6ffd96029484406b0c18
fanbu1995 <fb75@duke.edu>
2023-02-15 22:56:57 -0800

    save overlaid bias distribution plot code

--- extras/compareBiasDists.R
+++ extras/compareBiasDists.R
@@ -87,7 +87,7 @@ p
 db = 'CCAE'
 #me = 'SCCS'
 me = 'HistoricalComparator'
-aid = 2
+aid = 6
 pid = 9
 eid = 211983
 
@@ -158,6 +158,145 @@ p = ggplot(nullDat, aes(x=x)) +
         strip.text.y.left = element_text(angle = 0))
 )
 p
+
+
+# 02/13/2023: HC and SCCS overlay-----
+## (1) HC
+#db = 'IBM_MDCD'
+db = 'CCAE'
+#me = 'SCCS'
+me = 'HistoricalComparator'
+aid = 2
+pid = 9
+eid = 211983
+
+allExpos = readRDS('./localCache/exposures.rds')
+exposures_select = c(211983, 211833, 21184, 21185, 21215)
+
+nullDat = NULL
+NCDat = NULL 
+
+# databases = c('CCAE', 'IBM_MDCD', 'IBM_MDCR', 'OptumEhr', 'OptumDod')
+# alterNames = c('CCAE', 'MDCD', 'MDCR', 'OptumEHR', 'OptumDoD')
+
+for(ex in exposures_select){
+  eid = ex
+  # fit null dist.
+  fittedNull = fitNegativeControlDistributionLikelihood(connection,
+                                                        'eumaeus',
+                                                        database_id = db,
+                                                        method = me,
+                                                        analysis_id = aid,
+                                                        period_id = pid,
+                                                        exposure_id = eid,
+                                                        priorSds = c(0.5,0.5))
+  # pull MLEs for the NCs
+  NCs = fitNegativeControlDistribution(connection,
+                                       'eumaeus',
+                                       database_id = db,
+                                       method = me,
+                                       analysis_id = aid,
+                                       period_id = pid,
+                                       exposure_id = eid,
+                                       returnEstimatesOnly = TRUE) %>%
+    select(log_rr) %>% pull()
+  
+  # plot systematic error distribution
+  #this.db = alterNames[which(databases==dbId)]
+  this.ex = allExpos %>% filter(exposure_id == eid) %>% 
+    select(base_exposure_name) %>% pull()
+  nullDat = rbind(nullDat,
+                  data.frame(x=fittedNull$bias,
+                             exposure = this.ex))
+  NCDat = rbind(NCDat,
+                data.frame(x=NCs, y = 0.03,
+                           exposure = this.ex))
+}
+
+## (2) SCCS ----
+db = 'IBM_MDCD'
+db = 'CCAE'
+me = 'SCCS'
+#me = 'HistoricalComparator'
+aid = 2
+pid = 9
+eid = 211983
+
+allExpos = readRDS('./localCache/exposures.rds')
+exposures_select = c(211983, 211833, 21184, 21185, 21215)
+
+nullDat2 = NULL
+NCDat2 = NULL 
+
+# databases = c('CCAE', 'IBM_MDCD', 'IBM_MDCR', 'OptumEhr', 'OptumDod')
+# alterNames = c('CCAE', 'MDCD', 'MDCR', 'OptumEHR', 'OptumDoD')
+
+for(ex in exposures_select){
+  eid = ex
+  # fit null dist.
+  fittedNull = fitNegativeControlDistributionLikelihood(connection,
+                                                        'eumaeus',
+                                                        database_id = db,
+                                                        method = me,
+                                                        analysis_id = aid,
+                                                        period_id = pid,
+                                                        exposure_id = eid,
+                                                        priorSds = c(0.5,0.5))
+  # pull MLEs for the NCs
+  NCs = fitNegativeControlDistribution(connection,
+                                       'eumaeus',
+                                       database_id = db,
+                                       method = me,
+                                       analysis_id = aid,
+                                       period_id = pid,
+                                       exposure_id = eid,
+                                       returnEstimatesOnly = TRUE) %>%
+    select(log_rr) %>% pull()
+  
+  # plot systematic error distribution
+  #this.db = alterNames[which(databases==dbId)]
+  this.ex = allExpos %>% filter(exposure_id == eid) %>% 
+    select(base_exposure_name) %>% pull()
+  nullDat2 = rbind(nullDat2,
+                  data.frame(x=fittedNull$bias,
+                             exposure = this.ex))
+  NCDat2 = rbind(NCDat2,
+                data.frame(x=NCs, y = 0.03,
+                           exposure = this.ex))
+}
+
+## overlay the densities of bias distributions together----
+nullDatAll = rbind(nullDat %>% mutate(method = 'HistoricalComparator'), 
+                   nullDat2 %>% mutate(method = 'SCCS'))
+
+xlims = c(-2, 2)
+RRbreaks = c(0.2, 0.5, 1, 2, 5)
+xbreaks = log(RRbreaks)
+xlabels = RRbreaks %>% as.character()
+
+(
+  p3 = ggplot(nullDatAll, aes(x=x, fill = method)) +
+    geom_density(alpha = 0.4) +
+    # geom_point(data = NCDat, 
+    #            mapping = aes(x=x,y=y), shape = 4) +
+    scale_x_continuous(limits = xlims, 
+                       breaks = xbreaks, 
+                       labels = xlabels) +
+    labs(x='Rate ratio estimates for negative control outcomes',
+         y='',
+         fill = 'Design:') +
+    scale_y_continuous(breaks = NULL) +
+    scale_fill_manual(values = wes_palette("Darjeeling2")[2:3],
+                      labels = c('Historical Comparator', 
+                                 'Self-controlled'))+
+    theme_bw(base_size = 14) +
+    facet_grid(exposure~., switch = "y") + 
+    theme(strip.placement = "outside",
+          strip.text.y.left = element_text(angle = 0),
+          legend.position = 'right')
+)
+p3
+
 
 
 
