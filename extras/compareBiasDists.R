d4cccc56ea41a7021fd303134b67fca182d91b53
fanbu1995 <fb75@duke.edu>
2023-03-31 16:05:46 -0700

    add bias distribution plots over time for manuscript

--- extras/compareBiasDists.R
+++ extras/compareBiasDists.R
@@ -298,6 +298,88 @@ xlabels = RRbreaks %>% as.character()
 p3
 
 
+# 03/31/2023: plot NC MLEs and estimated null distributions over time  ----
+# for one exposure-database combo
+
+db = 'CCAE'
+#me = 'SCCS'
+me = 'HistoricalComparator'
+aid = 5
+pid = 9
+eid = 211983
+
+allPeriods = 1:12
+
+#allExpos = readRDS('./localCache/exposures.rds')
+#exposures_select = c(211983, 211833, 21184, 21185, 21215)
+
+nullDat = NULL
+NCDat = NULL 
+
+# databases = c('CCAE', 'IBM_MDCD', 'IBM_MDCR', 'OptumEhr', 'OptumDod')
+# alterNames = c('CCAE', 'MDCD', 'MDCR', 'OptumEHR', 'OptumDoD')
+
+for(pid in allPeriods){
+  # fit null dist.
+  fittedNull = fitNegativeControlDistributionLikelihood(connection,
+                                                        'eumaeus',
+                                                        database_id = db,
+                                                        method = me,
+                                                        analysis_id = aid,
+                                                        period_id = pid,
+                                                        exposure_id = eid,
+                                                        priorSds = c(0.5,0.5))
+  # pull MLEs for the NCs
+  NCs = fitNegativeControlDistribution(connection,
+                                       'eumaeus',
+                                       database_id = db,
+                                       method = me,
+                                       analysis_id = aid,
+                                       period_id = pid,
+                                       exposure_id = eid,
+                                       returnEstimatesOnly = TRUE) %>%
+    select(log_rr) %>% pull()
+  
+  # plot systematic error distribution
+  #this.db = alterNames[which(databases==dbId)]
+  nullDat = rbind(nullDat,
+                  data.frame(x=fittedNull$bias,
+                             period = pid))
+  NCDat = rbind(NCDat,
+                data.frame(x=NCs, y = 0.03, period = pid))
+}
+
+
+## some graphing setups
+xlims = c(-3, 3)
+RRbreaks = c(0.1, 0.5, 1, 2, 5, 20)
+xbreaks = log(RRbreaks)
+xlabels = RRbreaks %>% as.character()
+
+(
+  p = ggplot(nullDat, aes(x=x)) +
+    geom_vline(xintercept = 0, linetype = 2, 
+               linewidth = 1, color = 'gray40')+
+    geom_density(fill = 'gray80') +
+    geom_point(data = NCDat, 
+               mapping = aes(x=x,y=y), shape = 4) +
+    scale_x_continuous(limits = xlims, 
+                       breaks = xbreaks, 
+                       labels = xlabels) +
+    labs(x='Rate ratio estimates for negative control outcomes',
+         y='',
+         caption = 'Analysis time (month)') +
+    scale_y_continuous(breaks = NULL) +
+    coord_flip() +
+    theme_bw(base_size = 14) +
+    facet_grid(.~period, switch = "both") + 
+    theme(strip.placement = "inside",
+          strip.text.y.left = element_text(angle = 0),
+          panel.border = element_blank(),
+          strip.background = element_blank(),
+          plot.caption = element_text(size=14, hjust=0.5))
+)
+p
 
 
 # -----
