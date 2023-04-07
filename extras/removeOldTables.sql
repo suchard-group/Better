22ba5f28dacdd547b3ee3b7b16a9289c7469d08a
fanbu1995 <fb75@duke.edu>
2023-04-06 20:00:20 -0700

    sql to drop old tables

---
+++ extras/removeOldTables.sql
@@ -0,0 +1,11 @@
+-- Drop previous tables if exists
+
+DROP TABLE IF EXISTS analysis;
+DROP TABLE IF EXISTS exposures;
+DROP TABLE IF EXISTS all_mses;
+DROP TABLE IF EXISTS all_powers;
+DROP TABLE IF EXISTS priors;
+DROP TABLE IF EXISTS all_summary;
+DROP TABLE IF EXISTS all_tts;
+DROP TABLE IF EXISTS all_type1s;
+
