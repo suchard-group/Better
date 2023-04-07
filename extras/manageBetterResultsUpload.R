cebc76ec1f2f8d83bb6c0f60fe193a693f9ed886
fanbu1995 <fb75@duke.edu>
2023-04-06 20:00:38 -0700

    try using LegendT2dm results upload functionality

--- extras/manageBetterResultsUpload.R
+++ extras/manageBetterResultsUpload.R
@@ -1,4 +1,5 @@
 # Upload BETTER results to the OHDSI PostgreSQL public server
+exportFolder = '~/Documents/Research/betterOutput/export/'
 
 # set up connection details
 # using my credentials with write permissions.... 
@@ -13,4 +14,40 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(
 schema <- "better_results"
 
 # create results data models in the better_results schema
+# ONLY DO THIS ONCE!!!
 createResultsDataModel(connectionDetails, schema)
+
+# package up loose local csv's into a zip file for uploading
+zipName <- normalizePath(file.path(exportFolder, 'results_better.zip'), mustWork = FALSE)
+files <- list.files(exportFolder, pattern = ".*\\.csv$")
+oldWd <- getwd()
+setwd(exportFolder)
+#on.exit(setwd(oldWd))
+DatabaseConnector::createZipFile(zipFile = zipName, files = files)
+setwd(oldWd)
+
+# set 
+#Sys.setenv(POSTGRES_PATH = "/Library/PostgreSQL/11/bin")
+
+# Try Using legendT2dm functionality instead!
+LegendT2dm::uploadResultsToDatabase(
+  connectionDetails = connectionDetails,
+  schema = schema,
+  purgeSiteDataBeforeUploading = FALSE,
+  zipFileName = c(
+    zipName
+  ),
+  specifications = getResultsDataModelSpecifications()
+)
+
+# try uploading to ohdsi shinydb data server
+uploadResultsToDatabase(connectionDetails = connectionDetails,
+                        schema = schema,
+                        zipFileName = zipName,
+                        purgeSiteDataBeforeUploading = FALSE)
+
+## manual checking ----
+connection = DatabaseConnector::connect(connectionDetails)
+sql <- "SELECT COUNT(*) FROM better_results.summary"
+up_summary = DatabaseConnector::querySql(connection, sql) # summary wrong....
+
