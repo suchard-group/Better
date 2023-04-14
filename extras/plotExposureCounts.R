9c39365d8e252c0e027d6f8f0a341b5f3d77a338
fanbu1995 <fb75@duke.edu>
2023-04-14 08:13:00 -0700

    update exposure cohort counts plot (with CUIMC in)

--- extras/plotExposureCounts.R
+++ extras/plotExposureCounts.R
@@ -155,5 +155,69 @@ grid.arrange(grobs = plist,
              ncol = 2)
 
 
+# 04/13/2023: use data characterization results local copy & add CUIMC -----
+# still, one bit plot for exposure counts over time
+
+estimates = readRDS('./localCache/negativeControlEstimatesCharacteristics-2.rds')
+
+library(gridExtra)
+
+exposures = readRDS(file.path('./localCache/', 'exposures.rds'))
+theColors = c(wes_palette("Darjeeling1")[2:3], 
+              wes_palette("Darjeeling2")[2],
+              wes_palette("GrandBudapest1")[2],
+              wes_palette("IsleofDogs1")[1:2])
+
+exposed_counts = estimates %>% 
+  left_join(exposures, by = 'exposure_id') %>%
+  select(database_id, period_id, exposure_id, 
+         exposure_subjects, exposure_days,
+         exposure = base_exposure_name) %>% 
+  group_by(database_id, period_id, exposure) %>%
+  summarise(exposure_count = max(exposure_subjects)) %>%
+  ungroup()
+
+plist = list()
+overall_legend = NULL
+
+all_databases = unique(exposed_counts$database_id)
+for(db in all_databases){
+  this.exposed = exposed_counts %>% filter(database_id == db)
+  
+  if(stringr::str_starts(db,'IBM')){
+    dbname = unlist(stringr::str_split(db, '_'))[2]
+  }else{
+    dbname = db
+  }
+  
+  this.p = ggplot(this.exposed, aes(x = period_id, 
+                                y = exposure_count, 
+                                color = exposure)) +
+    geom_line(size = 1.2) +
+    scale_x_continuous(breaks = seq(from=3, to=12, by=3), limits = c(1, 12)) +
+    scale_y_log10(limits = c(1, 5*1e6))+
+    guides(color=guide_legend(nrow=6,byrow=TRUE))+
+    labs(x='Analysis period (in months)', y = 'Number of\nexposed subjects',
+         color = 'Vaccine exposure', title = sprintf('Database: %s',dbname)) +
+    theme_bw(base_size = 16) +
+    scale_color_manual(values = theColors)
+  
+  if(db == 'CCAE'){
+    overall_legend = lemon::g_legend(this.p+
+                                       theme(legend.position = 'right',
+                                             legend.text=element_text(size=12))+
+                                       guides(color=guide_legend(nrow=2,byrow=TRUE)))
+  }
+  this.p = this.p + theme(legend.position = 'hidden')
+  plist[[which(all_databases == db)]] = this.p
+}
+
+
+p_main = grid.arrange(grobs = plist,
+                      ncol = 2)
+
+grid.arrange(p_main, overall_legend, 
+             nrow = 2,
+             heights = c(8,1))
 
   
